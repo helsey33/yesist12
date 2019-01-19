9f31abaaeb4da0beb42f4f85bfa46e40fadac74f
helsey33 <helseysin827@gmail.com>
2019-01-19 09:03:22 +0530

    added responsive files

--- js/main.js
+++ js/main.js
@@ -14,3 +14,23 @@ window.addEventListener("load", () => {
     direction: "alternate"
   });
 });
+
+if (matchMedia) {
+  const mq = window.matchMedia("(max-width: 1000px)");
+  mq.addListener(WidthChange);
+  WidthChange(mq);
+}
+
+// media query change
+function WidthChange(mq) {
+  if (mq.matches) {
+    const logo = document.querySelector("#logo");
+    logo.src = "img/logo2.png";
+    logo.style.width = "80px";
+    logo.style.height = "80px";
+  } else {
+    logo.src = "img/logo1.png";
+    logo.style.width = "250px";
+    logo.style.height = "150px";
+  }
+}
